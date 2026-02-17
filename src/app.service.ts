04562dbfffcf5f2f9a80b71549d7384b58481413
Gustavo Costa <gu.triunfo@gmail.com>
2026-02-17 15:59:59 -0300

    feat: resolução usando IIFE e objeto mapa

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
   getHello(): string {
     return 'Hello World!';
   }
+
+  pagar(tipo: string): string {
+    const pagamentos: Record<string, string> = {
+      pix: 'Pagamento via Pix',
+      cartao: 'Pagamento via Cartão',
+      boleto: 'Pagamento via Boleto',
+    };
+
+    return pagamentos[tipo.toLowerCase()]
+      ?? (() => { throw new BadRequestException('Tipo inválido'); })()
+  }
+
 }
